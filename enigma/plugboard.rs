e91ddec48f26737216abf476c9b9c2696c115d26
agx-hv <133509826+agx-hv@users.noreply.github.com>
2023-08-04 15:19:20 +0800

    added modules

---
+++ enigma/plugboard.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+pub struct Plugboard {
+    pub n: u8, // Number of characters used
+    pub map: HashMap<u8,u8>, 
+}
+
+// Implementation of the Rotor Struct methods
+impl Plugboard {
+    pub fn new(mapping: &[(u8, u8)]) -> Plugboard {
+        let mut map: HashMap<u8,u8> = HashMap::new();
+        for t in mapping {
+            let (a, b) = t;
+            map.insert(*a, *b);
+            map.insert(*b, *a); 
+        }
+
+        return Plugboard {
+            n: mapping.len() as u8,
+            map
+        };
+    }
+
+
+    // Puts signal through plugboard and get new value
+    pub fn encipher(&self, input: u8) -> u8 {
+        // If no connection between two keys (wire), MAP back to same value
+        if !self.map.contains_key(&input) {
+            return input;
+        }
+        return self.map[&input];
+    }
+}
+
